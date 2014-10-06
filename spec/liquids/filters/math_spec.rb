d241dfc04c31c418b3e5ed68aa8e0acbec9d99b8
Maik Kempe <mkempe@bitaculous.com>
2014-10-06 10:06:48 +0200

    [✔] Fixed spec.

--- spec/liquids/filters/math_spec.rb
+++ spec/liquids/filters/math_spec.rb
@@ -2,7 +2,7 @@ describe Liquids::Filters::Math do
   include Liquids::Filters::Math
 
   describe '#mod' do
-    it 'it returns the correct number modulus' do
+    it 'returns the correct number modulus' do
       expect(mod(4, 4)).to eql 0
       expect(mod(4, 8)).to eql 4
       expect(mod(8, 4)).to eql 0
