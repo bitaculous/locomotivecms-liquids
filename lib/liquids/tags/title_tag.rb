e24111f743d54e111629685efe3545e718b8ecd4
Maik Kempe <mkempe@bitaculous.com>
2014-10-11 22:33:36 +0200

    [➠] First implementation of `title` tag.

--- lib/liquids/tags/title_tag.rb
+++ lib/liquids/tags/title_tag.rb
@@ -4,10 +4,26 @@ module Liquids
       # Register tag with name `title`
       tag_name :title
 
+      context_attribute :site
+      context_attribute :page
       context_attribute :wagon
 
-      def display
-        'Title'
+      def display(*options)
+        name   = site['name'] || site.name
+        title  = page.seo_title || page.title
+        handle = self.handleize page.handle || 'handle'
+
+        if handle == 'index'
+          "<title>#{name} | #{title}</title>"
+        else
+          "<title>#{title} | #{name}</title>"
+        end
+      end
+
+      protected
+
+      def handleize(input)
+        input.to_str.gsub(' ', '-').gsub('/', '-').downcase
       end
     end
   end
